5e566968a04e7cc31cb959bc92667075cdf99bb4
bluemoon2008 <asdfjkl2008h@gmail.com>
2022-07-19 16:33:36 +0800

    add ${fileName} for imageSaveFolder config
    Signed-off-by: bluemoon2008 <asdfjkl2008h@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -225,6 +225,7 @@ class EditorPanel {
                 vscode.workspace.getWorkspaceFolder(this._uri)?.uri.fsPath || ''
               )
               .replace('${file}', this._fsPath)
+              .replace('${fileName}', NodePath.basename(this._fsPath).split(".")[0])
               .replace('${dir}', NodePath.dirname(this._fsPath))
             const assetsFolder = NodePath.resolve(
               NodePath.dirname(this._fsPath),
